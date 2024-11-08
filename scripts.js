886d47617be94adaa67565624eec48476993b7cd
Nahar <sameul.nahar@EDU.LAN>
2024-11-08 14:13:47 +0100

    upload to remote repo

---
+++ scripts.js
@@ -0,0 +1,27 @@
+//Aufgabe 1
+
+const aufgabe1 = document.getElementById("clickme-button");
+
+aufgabe1.addEventListener("click", function() {
+alert('You clicked on the button!');
+});
+
+//Aufgabe 2
+
+const aufgabe2 = document.getElementById("date-button");
+
+
+aufgabe2.addEventListener("click", function(){
+    let currentDate = new Date();
+    currentDate.toLocaleDateString();
+});
+
+//Aufgabe 3
+
+
+
+//Aufgabe 4
+
+
+
+//Aufgabe 5
