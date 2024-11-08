5b500027f6ba2c5c6ac73388b16e9d54ebca9275
Nahar <sameul.nahar@EDU.LAN>
2024-11-08 16:25:44 +0100

    bis Aufgabe 3

--- scripts.js
+++ scripts.js
@@ -3,25 +3,42 @@
 const aufgabe1 = document.getElementById("clickme-button");
 
 aufgabe1.addEventListener("click", function() {
-alert('You clicked on the button!');
+  alert('Du hast einen Kuss erhalten <3');
 });
 
-//Aufgabe 2
-
+// Aufgabe 2
 const aufgabe2 = document.getElementById("date-button");
 
+aufgabe2.addEventListener("click", function() {
+  var currentDate = new Date();
+  var dateString = currentDate.toString();
+  
+  let inputDate = document.getElementById('inputDate');
+  let computedStyle = window.getComputedStyle(inputDate);
+  
+  // Check if the element is hidden by external CSS
+  if (computedStyle.display === 'none') {
+    inputDate.style.display = 'block';  // Make it visible
+    console.log("The element was hidden and is now shown.");
+  } 
+  
+  
 
-aufgabe2.addEventListener("click", function(){
-    let currentDate = new Date();
-    currentDate.toLocaleDateString();
+  inputDate.value = dateString; // Set the value of the input field to the current date
 });
 
 //Aufgabe 3
 
+document.getElementById("Stimmungen").addEventListener('change', function() {
+  const emotion = this.value;
 
-
-//Aufgabe 4
-
-
-
-//Aufgabe 5
+  if (emotion === '1') {
+    document.getElementById('Emotionszustand').textContent = "glücklich!";
+  } else if (emotion === '3') {
+    document.getElementById('Emotionszustand').textContent = "geärgert!";
+  } else if (emotion === '4') {
+    document.getElementById('Emotionszustand').textContent = "gelangweilt!";
+  } else if (emotion === '2')  {
+    document.getElementById("Emotionszustand").textContent = "traurig!"
+  }
+});
